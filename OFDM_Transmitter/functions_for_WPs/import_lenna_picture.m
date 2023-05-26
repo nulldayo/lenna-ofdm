e229c96372102b36283f2ee17714cc577e688492
nulldayo <66908818+nulldayo@users.noreply.github.com>
2023-05-26 11:05:59 +0200

    Implemented import_lenna_picture.m

--- OFDM_Transmitter/functions_for_WPs/import_lenna_picture.m
+++ OFDM_Transmitter/functions_for_WPs/import_lenna_picture.m
@@ -4,6 +4,15 @@ function lenna_symbols = import_lenna_picture(mapping)
 % Input:    mapping (created by generate mapping in framework)
 % Output:   lenna_symbols -> 36864x1 complex double 
 
+im = imread('LENNA_Scaled96.JPG','JPG'); %96x96 intensity values,0-255
+imserial = im(:); %columns of im in series
+
+imserial_bin = de2bi(imserial); %convert to binary
+imserial_bin = imserial_bin.';
+imserial_bin = imserial_bin(:); %-> serialise values by line is one value
+
+lenna_symbols = map_symbol(imserial_bin, mapping); %map binary vector to QAM symbols
+lenna_symbols = lenna_symbols(:); %return lenna_symbols as column vector
 
 end
 
