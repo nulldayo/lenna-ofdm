63ec1994971ad40fd9ba309f4926c691566ea09c
Icepoenix <mrnospm@gmail.com>
2023-06-01 09:28:28 +0200

    Optimized rearrange_data() for better performance

--- OFDM_Transmitter/functions_for_WPs/rearrange_data.m
+++ OFDM_Transmitter/functions_for_WPs/rearrange_data.m
@@ -11,8 +11,7 @@ data_freq_matrix=zeros(N_OFDM_symbols,parameter.Nfft);
 if ~mod(length(lenna_symbols)/length(parameter.Nused_data_idx),1)==0 % If N lenna symbols is not integer multiple of symbols per OFDM symbol
     lenna_symbols=[lenna_symbols;zeros(N_OFDM_symbols*length(parameter.Nused_data_idx)-length(lenna_symbols),1)];% Add Zeros to adjust length
 end
-for i=1:N_OFDM_symbols % for each row of data containing one OFDM symbol
-    data_freq_matrix(i,parameter.Nused_data_idx)=lenna_symbols(((i-1)*length(parameter.Nused_data_idx)+1):(i*length(parameter.Nused_data_idx)));
-end
+data_freq_matrix(:,parameter.Nused_data_idx)=reshape(lenna_symbols,length(parameter.Nused_data_idx),N_OFDM_symbols).';% Insert lenna_symbols into matrix rows at specified columns
+                                                                                                                      % Note: reshape fills by column -> transpose and swapped reshape arguments
 end
 
