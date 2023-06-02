78c3755c999274cb27591d7d1ecf7dc0875f4334
Icepoenix <mrnospm@gmail.com>
2023-06-02 09:49:45 +0200

    Made Testfunction into function rather than m file

--- Testfunctions.m
+++ Testfunctions.m
@@ -1,7 +1,7 @@
 %% Runs all WP functions and compares them to the _p functions
 
 %add paths first
-
+function []=Testfunctions()
 %% -------------Initialization of different parameters---------------------
 %framework function to define parameters
 parameter = predefine_parameter();
@@ -112,6 +112,7 @@ if isequal(frames,frames_WP)
     disp('WP6 successfull');
 else
     disp('WP6 unsuccessfull');
+end
 end
 
 
