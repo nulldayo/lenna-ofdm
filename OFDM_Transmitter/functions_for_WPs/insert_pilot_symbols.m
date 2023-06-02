eb64ff30344afdd8c173f913fafa633df897fcce
nulldayo <mk.mizutani1227@gmail.com>
2023-06-02 09:47:02 +0200

    implemented WP3

--- OFDM_Transmitter/functions_for_WPs/insert_pilot_symbols.m
+++ OFDM_Transmitter/functions_for_WPs/insert_pilot_symbols.m
@@ -5,7 +5,7 @@ function data_frec_with_pilot = insert_pilot_symbols(data_freq_matrix,training_f
 %           training_freq -> training data in frequency domain
 %           parameter -> struct with predefined OFDM parameter
 % Output:   data_frec_with_pilot -> 768x64 complex double
-
-
+data_freq_matrix(:,parameter.Nused_pilot_idx)=repmat(training_freq(parameter.Nused_pilot_idx),size(data_freq_matrix,1),1);
+data_frec_with_pilot=data_freq_matrix;
 end
 
