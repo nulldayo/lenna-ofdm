d82b6cdedee04fe6c73322d48b3cc55119bec165
Icepoenix <mrnospm@gmail.com>
2023-06-05 15:06:14 +0200

    Implemented WP11

--- OFDM_Receiver/functions_for_WPs/transform_to_frequency_domain.m
+++ OFDM_Receiver/functions_for_WPs/transform_to_frequency_domain.m
@@ -6,6 +6,6 @@ function frame_freq = transform_to_frequency_domain(frame_noGuard, parameter)
 % Input:    frame_noGuard -> 64x131 complex double
 %           parameter -> struct with predefined OFDM parameter
 % Output:   frame_noGuard -> 64x131 complex double
-
+frame_freq=fft(frame_noGuard,parameter.Nfft,1)/sqrt(parameter.Nfft);
 end
 
