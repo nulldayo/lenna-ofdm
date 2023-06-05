d9a6e56e72d54fc27783e8b692ce5d264d80e152
Icepoenix <mrnospm@gmail.com>
2023-06-05 15:14:15 +0200

    Implemented WP12

--- OFDM_Receiver/functions_for_WPs/channel_estimation.m
+++ OFDM_Receiver/functions_for_WPs/channel_estimation.m
@@ -1,10 +1,10 @@
-function channel_coefficiants = channel_estimation(frame_freq, training_freq)
+function channel_coefficients = channel_estimation(frame_freq, training_freq)
 %CHANNEL_ESTIMATION This function estimates the channel coefficiants using training symbols
 %
 % Input:    frame_freq -> 64x131 complex double
 %           training_freq -> training data in frequency domain
 % Output:   channel_coefficiants -> 64x1 complex double
-
+channel_coefficients=frame_freq(:,1)./training_freq(:);
 
 end
 
