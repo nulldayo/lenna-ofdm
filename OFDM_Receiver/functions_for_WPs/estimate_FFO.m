c9378510245133243957302451da4658c1b654f4
nulldayo <mk.mizutani1227@gmail.com>
2023-06-05 13:26:26 +0200

    Implemented WP8 with for loops

--- OFDM_Receiver/functions_for_WPs/estimate_FFO.m
+++ OFDM_Receiver/functions_for_WPs/estimate_FFO.m
@@ -6,6 +6,13 @@ function FFO_est = estimate_FFO(frame_with_guard, parameter)
 %           parameter -> struct with predefined OFDM parameter
 % Output:   FFO_est -> 1x1 double
 
+FFO_arg = 0;
 
+for k=1:size(frame_with_guard,1) % for each ofdm symbol
+    for mu=1:parameter.Nguard
+        FFO_arg = FFO_arg + frame_with_guard(mu,k)*(frame_with_guard(mu+parameter.Nfft,k)');
+    end
 end
 
+FFO_est = (-1/(2*pi))*phase(FFO_arg);
+end
