9cd823d304d2214cd0b51b3728bf638e2f82a106
Icepoenix <mrnospm@gmail.com>
2023-06-05 13:19:42 +0200

    implemented WP8

--- OFDM_Receiver/functions_for_WPs/estimate_FFO.m
+++ OFDM_Receiver/functions_for_WPs/estimate_FFO.m
@@ -5,7 +5,9 @@ function FFO_est = estimate_FFO(frame_with_guard, parameter)
 % Input:    frame_with_guard -> 80x131 complex double
 %           parameter -> struct with predefined OFDM parameter
 % Output:   FFO_est -> 1x1 double
-
+guard=frame_with_guard(1:parameter.Nguard,:);
+frame_end=frame_with_guard(end-parameter.Nguard+1:end,:);
+FFO_est=-1/(2*pi)*phase(sum(guard.*conj(frame_end),'all'));
 
 end
 
