d4cbfa45e389de08eb123a037b6a02e3f9012199
Icepoenix <mrnospm@gmail.com>
2023-06-23 09:33:56 +0200

    Implemented WP15

--- OFDM_Receiver/functions_for_WPs/classify_frame.m
+++ OFDM_Receiver/functions_for_WPs/classify_frame.m
@@ -10,5 +10,13 @@ function lenna_frames = classify_frame(lenna_frames, frame_no_RFO, mapping, para
 % Output:   lenna_frames -> cell array with lenna frames in correct order
 %                        -> a used cell contains {48x129 complex double}
 
+
+header_bin=demap_symbol(frame_no_RFO(1:2,1),mapping);
+%header_bin(4)=[]; % uncomment for testing function, given p function only
+%uses the first 3 bits
+header_dec=bi2de_(header_bin);
+lenna_frames(header_dec)={frame_no_RFO(:,2:end)};
+
+
 end
 
