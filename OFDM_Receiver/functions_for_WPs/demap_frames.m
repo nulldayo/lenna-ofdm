fe7861b1e467d5c7d7e97803091fb5ab42cc6201
Icepoenix <mrnospm@gmail.com>
2023-06-23 10:04:36 +0200

    Implemented WP16

--- OFDM_Receiver/functions_for_WPs/demap_frames.m
+++ OFDM_Receiver/functions_for_WPs/demap_frames.m
@@ -4,7 +4,13 @@ function lenna_binary = demap_frames(lenna_frames, mapping)
 % Input:    lenna_frames -> cell array with lenna frames in correct order
 %           mapping -> mapping (created by generate mapping in framework)
 % Output:   lenna_binary -> 1x74304 logical
-
+lenna_binary=false(1,mapping.m*numel(lenna_frames{1})*length(lenna_frames));
+for i=1:length(lenna_frames)
+    frame=lenna_frames{i};
+    frame=frame(:); % convert frame matrix to column vector
+    frame_bin=demap_symbol(frame,mapping);
+    lenna_binary(1,(i-1)*mapping.m*numel(lenna_frames{1})+1:i*mapping.m*numel(lenna_frames{1}))=frame_bin;
+end
 
 end
 
