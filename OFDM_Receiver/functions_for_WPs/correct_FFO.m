44db4562b395248ec9763a342c3a7d846086f2f6
Icepoenix <mrnospm@gmail.com>
2023-06-27 20:02:29 +0200

    Implemented WP9

--- OFDM_Receiver/functions_for_WPs/correct_FFO.m
+++ OFDM_Receiver/functions_for_WPs/correct_FFO.m
@@ -6,7 +6,17 @@ function frame_with_guard_noFFO = correct_FFO(correctFFO, frame_with_guard, FFO_
 %           FFO_est -> 1x1 double
 %           parameter -> struct with predefined OFDM parameter
 % Output:   frame_with_guard_noFFO -> 80x131 complex double
-
+if correctFFO
+N=parameter.Nfft;
+% k=(1:parameter.Nofdm+1);
+% i=(-parameter.Nguard:N-1).';
+% l=repmat(k,N+parameter.Nguard,1)*(N+parameter.Nguard)+repmat(i,1,parameter.Nofdm+1)+1;
+l=reshape(1:(80*131),80,131);
+phaseshift=exp(-1j*2*pi*FFO_est*l/N);
+frame_with_guard_noFFO=frame_with_guard.*phaseshift;
+else
+    frame_with_guard_noFFO=frame_with_guard;
+end
 
 end
 
